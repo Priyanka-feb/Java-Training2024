25634c3dc48a8a8ca1e29ec5da3c742e91ae5e5e
Priyanka-feb <Priyankappatel20@gmail.com>
2024-09-27 15:55:10 -0700

    Insert Table

---
+++ SQL Project/src/main/java/com/learn/sqltable.java
@@ -0,0 +1,50 @@
+package com.learn;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.mysql.cj.jdbc.JdbcConnection;
+
+/**
+ * Servlet implementation class sqltable
+ */
+@WebServlet("/sqltable")
+public class sqltable extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public sqltable() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newclient","root","Newuser");
+			Statement statement = connection.createStatement();
+				statement.executeUpdate("insert into library values (4,'Pritesh', 'Patel', 'Mountain House')");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		     catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
